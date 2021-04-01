66e9196a2e3e4b9a79c2e9f5aed04a5f9d016c72
komalb1409 <81753274+komalb1409@users.noreply.github.com>
2021-04-01 17:25:11 +0530

    2D game

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+using Debug = UnityEngine.Debug;
+using Random = UnityEngine.Random;
+
+public class GameController : MonoBehaviour
+{
+    private int num;
+   // private int guessNumber;
+
+    private int countGuess;
+
+    [SerializeField]
+    private GameObject btn;
+
+    [SerializeField]
+    private InputField input;
+
+    [SerializeField]
+    private Text text;
+
+    void Awake()
+    {
+          input = GameObject.Find("InputField").GetComponent<InputField>();
+        num = Random.Range(0, 100);
+        text.text = " Guess A Number Between 0 and 100 ";
+    }
+   public void GetInput(string guess)
+    {
+        CompareGuesses(int.Parse(guess));
+        input.text = "";
+        countGuess++;
+    }
+
+    void CompareGuesses(int guess)
+    {
+        if(guess == num)
+        {
+            text.text = "You Guessed Correctly The Number was " + guess + " It took you "+ countGuess + " Guessess. " + "Do you want to Play again?" ;
+            btn.SetActive(true);
+        }
+        else if(guess < num)
+        {
+            text.text = "You Number is less than my Number. Please try again! ";
+
+        }
+        else if (guess > num)
+        {
+            text.text = "You Number is greater than my Number. Please try again! ";
+        }
+
+
+    }
+
+    public void PlayAgain()
+    {
+        num = Random.Range(0, 100);
+        text.text = " Guess A Number Between 0 and 100 ";
+        countGuess = 0;
+        btn.SetActive(false);
+
+    }
+}
